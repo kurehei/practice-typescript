19209e49ca412b7778416f9dbfca62a1a1a11685
榑林晃平 <kureykou321@gmail.com>
2021-12-05 22:05:56 +0900

    メソッドに型をつけるようにした

--- hello.js
+++
@@ -1,2 +0,0 @@
-var message = "hello! Nekousho!!";
-console.log(message);
---
+++ practice.ts
@@ -0,0 +1,6 @@
+const score: number = 1;
+
+function add(a:number, b:number): number {
+  return a + b;
+}
+console.log(add(score, 2))
